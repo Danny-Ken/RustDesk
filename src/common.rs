7583cfbece11aac9db334ad6d33b0517cbbb2837
Danny-Ken <98000615+Danny-Ken@users.noreply.github.com>
2025-05-21 17:55:11 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -960,10 +960,7 @@ pub fn get_custom_rendezvous_server(custom: String) -> String {
 
 #[inline]
 pub fn get_api_server(api: String, custom: String) -> String {
-    let mut res = get_api_server_(api, custom);
-    if res.ends_with('/') {
-        res.pop();
-    }
+    let res = get_api_server_(api, custom);
     if res.starts_with("https") && res.ends_with(":21114") {
         return res.replace(":21114", "");
     }
@@ -994,11 +991,6 @@ fn get_api_server_(api: String, custom: String) -> String {
         }
     }
     "https://admin.rustdesk.com".to_owned()
-}
-
-#[inline]
-pub fn is_public(url: &str) -> bool {
-    url.contains("rustdesk.com")
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
